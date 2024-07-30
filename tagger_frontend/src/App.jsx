f78446a0c99ee039c1b9eaf1b70ac70c2c0db236
Somya Chawla <somyachawla16@gmail.com>
2024-07-30 19:03:11 +0530

    JSX Vite

--- tagger_frontend/src/App.jsx
+++ tagger_frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import BuyerForm from './page/buyer'
 import './App.css'
+import Buyer from './page/buyer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        
+        <Buyer/>
       </div>  
     </>
   )
